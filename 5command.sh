414e101f3b9e396bfbc4acaef38c2dedbe009fff
root <root@neeraj.compas.cs.stonybrook.edu>
2016-10-13 13:28:37 -0400

    added stable result for SLAB web search benchmark

--- 5command.sh
+++ 5command.sh
@@ -1,6 +1,8 @@
 for run in {1..6}
 do
-  docker run --rm --volumes-from data cloudsuite/in-memory-analytics /data/ml-latest-small /data/myratings.csv
+  #docker run --rm --volumes-from data cloudsuite/in-memory-analytics /data/ml-latest-small /data/myratings.csv
+  docker rm -v $(docker ps -a -q -f status=exited)
+  docker run -it --name client --net search_network cloudsuite/web-search:client 172.18.0.2 50 90 60 60
   if [ $run -eq 5 ]
   then
     echo "LAST RUN, TURN ON SCRIPT ..."
