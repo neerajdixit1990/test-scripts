deca267d6f2c7010f4d408d38321d0606b1ae17e
root <root@neeraj.compas.cs.stonybrook.edu>
2016-10-14 02:57:25 -0400

    added stable results for SLUB inmemory and web search benchmarks

--- 5command.sh
+++ 5command.sh
@@ -1,7 +1,7 @@
 for run in {1..6}
 do
-  #docker run --rm --volumes-from data cloudsuite/in-memory-analytics /data/ml-latest-small /data/myratings.csv
   docker rm -v $(docker ps -a -q -f status=exited)
+  #docker run --rm --volumes-from data cloudsuite/in-memory-analytics /data/ml-latest-small /data/myratings.csv
   docker run -it --name client --net search_network cloudsuite/web-search:client 172.18.0.2 50 90 60 60
   if [ $run -eq 5 ]
   then
