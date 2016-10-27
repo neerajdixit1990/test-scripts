b8315c798601684f3695a903042327a93095f418
root <root@neeraj.compas.cs.stonybrook.edu>
2016-10-27 17:37:52 -0400

    addred crashed result for SLAB graph analytics bench mark

--- 5command.sh
+++ 5command.sh
@@ -2,7 +2,8 @@ for run in {1..6}
 do
   docker rm -v $(docker ps -a -q -f status=exited)
   #docker run --rm --volumes-from data cloudsuite/in-memory-analytics /data/ml-latest-small /data/myratings.csv
-  docker run -it --name client --net search_network cloudsuite/web-search:client 172.18.0.2 50 90 60 60
+  #docker run -it --name client --net search_network cloudsuite/web-search:client 172.18.0.2 50 90 60 60
+  docker run --rm --volumes-from data cloudsuite/graph-analytics --driver-memory 16g --executor-memory 16g
   if [ $run -eq 5 ]
   then
     echo "LAST RUN, TURN ON SCRIPT ..."
