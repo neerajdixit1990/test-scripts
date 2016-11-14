8f363b4418dc7a8c9c279a64d3e84c517cd05704
root <root@neeraj.compas.cs.stonybrook.edu>
2016-11-14 17:59:04 -0500

    change script for taking stable readings to add simple benchmark

--- 5command.sh
+++ 5command.sh
@@ -1,7 +1,8 @@
-for run in {1..16}
+for run in {1..6}
 do
-  docker rm -v $(docker ps -a -q -f status=exited)
-  docker run --rm --volumes-from data cloudsuite/in-memory-analytics /data/ml-latest-small /data/myratings.csv
+  time ./simple_benchmark 400  
+  #docker rm -v $(docker ps -a -q -f status=exited)
+  #docker run --rm --volumes-from data cloudsuite/in-memory-analytics /data/ml-latest-small /data/myratings.csv
   #docker run -it --name client --net search_network cloudsuite/web-search:client 172.18.0.2 50 90 60 60
   #docker run --rm --volumes-from data cloudsuite/graph-analytics --driver-memory 16g --executor-memory 16g
   if [ $run -eq 5 ]
