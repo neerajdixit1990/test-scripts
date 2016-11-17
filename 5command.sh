468b36a2ff076b718f8b8a9587b0127c017e8269
root <root@neeraj.compas.cs.stonybrook.edu>
2016-11-17 15:50:25 -0500

    added results for stable slub simple benchmark

--- 5command.sh
+++ 5command.sh
@@ -1,6 +1,7 @@
-for run in {1..6}
+for run in {1..205}
 do
-  time ./simple_benchmark 400  
+  # 390 is the magic number where the si & so are 0 for complete duration of benchmark
+  time ./simple_benchmark 390
   #docker rm -v $(docker ps -a -q -f status=exited)
   #docker run --rm --volumes-from data cloudsuite/in-memory-analytics /data/ml-latest-small /data/myratings.csv
   #docker run -it --name client --net search_network cloudsuite/web-search:client 172.18.0.2 50 90 60 60
