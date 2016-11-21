92fd172a0033a481f8c9bb6e739edac23397c1d3
root <root@neeraj.compas.cs.stonybrook.edu>
2016-11-21 01:26:58 -0500

    added code for changing CPU for processes

--- simple_benchmark.c
+++ simple_benchmark.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <sys/mman.h> 
 #include <unistd.h>
@@ -6,12 +7,42 @@
 #include <sys/wait.h>
 #include <sched.h>
 #include <errno.h>
-#define	N 10000
+
+#define	N 		10000
+#define STEP		100
+
+void change_cpu() {
+	int		i = 0, j = 0;
+	cpu_set_t       mask;
+	int		status;
+
+	// for every 100th memory access, get CPU affinity
+	CPU_ZERO(&mask);
+	status = sched_getaffinity(0, sizeof(mask), &mask);
+	if (status != 0)
+		printf("\nUnable to get CPU affinity: %s\n", strerror(errno));
+
+	for (j = 0; j < 8; j++) {
+		if (CPU_ISSET(j, &mask))
+		break;
+	}
+
+	// change CPU affinity to next CPU
+	CPU_ZERO(&mask);
+	CPU_SET((j+1)%8, &mask);
+	status = sched_setaffinity(0, sizeof(mask), &mask);
+	if (status != 0)
+		printf("\nUnable to set CPU affinity: %s\n", strerror(errno));
+
+	// yield to access memory from different CPU
+	status = sched_yield();
+	if (status != 0)
+		printf("\nUnable to yield CPU : %s\n", strerror(errno));
+}
 
 int main(int argc, char **argv) {
         int     	depth = 0;
 	int		status = -1;
-	cpu_set_t  	mask;
         int     	i = 0;
         void    	*ret_addr[N];
         int     	*int_ptr = NULL;
@@ -23,19 +54,15 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
         depth = atoi(argv[1]);
-	// set a CPU for the current process to ensure that the processes are not
- 	// scheduled on the same CPU
-	CPU_ZERO(&mask);
-	CPU_SET(depth%8, &mask);
-	status = sched_setaffinity(0, sizeof(mask), &mask);
-	if (status != 0)
-		printf("\nUnable to set CPU affinity: %s\n", strerror(errno));	
-
 	snprintf(args, 10, "%d", depth-1);
 
 	// mmap anonymous memory in the parent process
-	for (i = 0; i < N; i++)
+	for (i = 0; i < N; i++) {
 		ret_addr[i] = mmap(0, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+		
+		if (i%STEP == 0)
+			change_cpu();
+	}
 
 	// access that memory to fill up the page tables
 	for (i = 0; i < N; i++) {
@@ -51,12 +78,15 @@ int main(int argc, char **argv) {
 			printf("\nShould not return !!\n");
 			return 0;
 		} else if (pid < 0) {
-			printf("\nError in fork() system call !\n");
+			printf("\nError in fork() system call : %s\n", strerror(errno));
 		} else {
                         // make separate pages in-case of COW fork()
                         for (i = 0; i < N; i++) {
                                 int_ptr = ret_addr[i];
                                 *int_ptr = 102;
+				
+				if (i%STEP == 0)
+					change_cpu();	
                         }
 			// parent process waits for child completion
 			while (wait(&status) != pid) 
@@ -65,7 +95,11 @@ int main(int argc, char **argv) {
 	}
 
         // unmap memory in parent process
-	for (i = 0; i < N; i++)
+	for (i = 0; i < N; i++) {
                 status = munmap(ret_addr[i], 4096);
+
+		if (i%STEP == 0)
+			change_cpu();	
+	}
 	return 0;
 }
