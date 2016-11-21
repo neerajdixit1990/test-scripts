73b397fd5c9a7c715f0cf6b723b7be416ae1a437
root <root@neeraj.compas.cs.stonybrook.edu>
2016-11-20 20:43:45 -0500

    added scheduler affinity to each process to ensure distribution of processes

--- simple_benchmark.c
+++ simple_benchmark.c
@@ -4,22 +4,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-
+#include <sched.h>
+#include <errno.h>
 #define	N 10000
+
 int main(int argc, char **argv) {
-        int     depth = 0;
+        int     	depth = 0;
+	int		status = -1;
+	cpu_set_t  	mask;
+        int     	i = 0;
+        void    	*ret_addr[N];
+        int     	*int_ptr = NULL;
+        pid_t   	pid = 0;
+        char    	args[10];
+
 	if (argc < 2) {
 		printf("Usage: <prog> <num of forks>\n");
 		exit(1);
 	}
         depth = atoi(argv[1]);
-
-	int	i = 0;
-	void 	*ret_addr[N];
-	int	*int_ptr = NULL;
-	pid_t	pid = 0;
-	char	args[10];
-	int	status = 0;
+	// set a CPU for the current process to ensure that the processes are not
+ 	// scheduled on the same CPU
+	CPU_ZERO(&mask);
+	CPU_SET(depth%8, &mask);
+	status = sched_setaffinity(0, sizeof(mask), &mask);
+	if (status != 0)
+		printf("\nUnable to set CPU affinity: %s\n", strerror(errno));	
 
 	snprintf(args, 10, "%d", depth-1);
 
